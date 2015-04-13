568f1b84050e72d3ec004e4bb2d4355eb8345e0e
Raincle <yaweihong@icloud.com>
2015-04-13 22:46:09 +0800

    longPressToRestart

--- Extensions.swift
+++ Extensions.swift
@@ -17,6 +17,8 @@ extension CGFloat {
 }
 
 
+
+
 extension UIColor {
     class var randomColor: UIColor {
         switch arc4random()%5 {
@@ -29,3 +31,16 @@ extension UIColor {
         }
     }
 }
+
+//extension UIColor {
+//    class var randomColor: UIColor {
+//        switch arc4random()%5 {
+//        case 0: return UIColor(red: CGFloat(255.0/255.0), green: CGFloat(110.0/255.0), blue: CGFloat(135.0/255.0), alpha: 1)
+//        case 1: return UIColor(red: CGFloat(205.0/255.0), green: CGFloat(115.0/255.0), blue: CGFloat(230.0/255.0), alpha: 1)
+//        case 2: return UIColor(red: CGFloat(150.0/255.0), green: CGFloat(140.0/255.0), blue: CGFloat(255.0/255.0), alpha: 1)
+//        case 3: return UIColor(red: CGFloat(115.0/255.0), green: CGFloat(175.0/255.0), blue: CGFloat(230.0/255.0), alpha: 1)
+//        case 4: return UIColor(red: CGFloat(125.0/255.0), green: CGFloat(255.0/255.0), blue: CGFloat(235.0/255.0), alpha: 1)
+//        default:return UIColor.whiteColor()
+//        }
+//    }
+//}
--- GravityViewController.swift
+++ GravityViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class GravityViewController: UIViewController
 {
 
-    @IBOutlet weak var AnimationView: UIView!
+    @IBOutlet weak var startLabel: UILabel!
     
+    @IBOutlet weak var AnimationView: UIView!
+    var hasShowAlert = false
+
     var dropsPerRow = 10
     var dropSize: CGSize {
         let size = AnimationView.bounds.width / CGFloat(dropsPerRow)
@@ -41,6 +44,16 @@ class GravityViewController: UIViewController
         animator.addBehavior(collider)
     }
     @IBAction func dropGestrue(sender: UITapGestureRecognizer) {
+        if startLabel.alpha == 1 {
+            for i in 1...36 {
+                drop()
+            }
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDuration(1)
+            startLabel.alpha = 0
+            UIView.commitAnimations()
+            
+        }
         
         drop()
     }
@@ -50,12 +63,44 @@ class GravityViewController: UIViewController
         frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
         
         var dropView = UIView(frame: frame)
-        dropView.backgroundColor = UIColor.randomColor
+        var color = UIColor.randomColor
+
+        dropView.backgroundColor = color
+        dropView.layer.cornerRadius = 10
+        dropView.layer.masksToBounds = true
+        dropView.layer.borderWidth = 1
         
         AnimationView.addSubview(dropView)
         gravity.addItem(dropView)
         collider.addItem(dropView)
     }
     
+    
+    
+    @IBAction func longPress(sender: UILongPressGestureRecognizer) {
+        if !hasShowAlert {
+            var alert = UIAlertController(title: "Restart", message: "Would you like to restart?", preferredStyle: UIAlertControllerStyle.Alert)
+            var okAction = UIAlertAction(title: "OK", style: .Default) { (action:UIAlertAction!) -> Void in
+                self.hasShowAlert = false
+                self.startLabel.alpha = 1
+                for view in self.AnimationView.subviews {
+                    if view.tag == 0 {
+                        self.gravity.removeItem(view as! UIDynamicItem)
+                        self.collider.removeItem(view as! UIDynamicItem)
+                        view.removeFromSuperview()
+                    }
+                }
+            }
+            var cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+                action in
+                self.hasShowAlert = false
+            }
+            alert.addAction(cancelAction)
+            alert.addAction(okAction)
+            presentViewController(alert, animated: true, completion: { () -> Void in
+                self.hasShowAlert = true
+            })
+        }
+    }
 }
 
